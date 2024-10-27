cd118855b269984531ef883fa7d9b67aaf4bb932
LeXuanVu <xuanvujame@gmail.com>
2024-10-27 16:49:35 +0700

    update full frontend

--- Frontend/timkiemvieclam/src/pages/QLNguoiDung/qlnguoidung.jsx
+++ Frontend/timkiemvieclam/src/pages/QLNguoiDung/qlnguoidung.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import MainLayout from "../mainLayout";
+import FormDangky from "./formDangKy";
+import FormDanhSach from "./formDanhsach";
+import "./qlnguoidung.scss";
+import FormCapNhat from "./formCapNhat";
 
 
 const QlNguoiDung = () => {
+    const [modalAdmin, setModalAdmin] = useState(false)
+    const [update, setUpdate] = useState(false);
+    const handleClickmodal =()=>{
+        setModalAdmin(!modalAdmin);
+        setUpdate(false)
+    }
+
     return(
         <>
-        <MainLayout><p>Ql nguoi QlNguoiDung</p>
-        </MainLayout>
+            <MainLayout>
+                <div className="qlnguoidung">
+                      <FormDanhSach update={update} setUpdate={setUpdate} modalAdmin={modalAdmin} setModalAdmin={setModalAdmin}  />
+                      <button onClick={handleClickmodal}>Tạo tài khoản</button>
+                      {modalAdmin ?  
+                      <div className="modal">
+                        <div className="formModal">
+                            <span onClick={() => setModalAdmin(!modalAdmin)} class="close-button">&times;</span>
+                            {update ? <FormCapNhat/> : <FormDangky/>}
+                            
+                            
+                        </div>
+                      </div>
+                      : ""}
+                </div>
+            </MainLayout>
         </>
     )
 }
